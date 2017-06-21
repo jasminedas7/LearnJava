76c60935f12753f1808f1f16b855cc5a1013edec
jasmine das <jasmine das7>
2017-06-21 22:24:06 +0530

    Sum of elements in array.[C

---
+++ ArraySum.java
@@ -0,0 +1,14 @@
+class ArraySum
+{
+	public static void main(String args[])
+	{
+		int sum=0;
+		int num[]={1,2,3,4,5};
+		for(int i=0;i<5;i++)
+		{
+			sum=sum+num[i];
+			
+		}
+		System.out.println(sum);
+	}
+}
