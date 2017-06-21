d4944de90962c059f3a6de3143c31c40019062e7
jasmine das <jasmine das7>
2017-06-21 22:41:16 +0530

    Find the divisibility factors of a number and print the factors.

---
+++ DivisibilityOf Number.java
@@ -0,0 +1,15 @@
+class DivisibilityOfNumber
+{
+	public static void main(String args[])
+{
+	int x=506;
+	for(int i=1;i<=9;i++)
+	{
+		if(x%i==0)
+		{
+			System.out.println(i);
+
+		}
+	}
+}
+}
---
+++ DivisibilityOfNumber.java
@@ -0,0 +1,15 @@
+class DivisibilityOfNumber
+{
+	public static void main(String args[])
+{
+	int x=111;
+	for(int i=1;i<=10;i++)
+	{
+		if(x%i==0)
+		{
+			System.out.println(i);
+
+		}
+	}
+}
+}
