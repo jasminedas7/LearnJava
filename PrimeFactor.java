c8d96b2e58302b134d5e67e579fbfad77b651c49
jasmine das <jasmine das7>
2017-06-24 13:31:27 +0530

    Print 2 primefactors separately

--- PrimeFactor.java
+++ PrimeFactor.java
@@ -1,20 +1,39 @@
 class PrimeFactor {
 	public static void main(String args[]) {
-		int i,num=24,index=0;
+
+		int i,num1=22,num2=36,index1=0;
 		int factor[]=new int[100];
+		int num=num1;
 		while(num!=1) { 
 			for(i=2;i<=num;i++) {
 	            if(num%i==0) {
-					System.out.println(i);
-					factor[index]=i;
-					index++;
+					factor[index1]=i;
+					index1++;
 					num=num/i;
 					break;
 		        }
         	}
 		}
-		for(int j=0;j<index;j++){
+		int index2=0;
+		int factorOf36[]=new int[200];
+		num=num2;
+		while(num!=1) { 
+			for(i=2;i<=num;i++) {
+	            if(num%i==0) {
+					factorOf36[index2]=i;
+					index2++;
+					num=num/i;
+					break;
+		        }
+        	}
+		}
+		System.out.println("PRIME FACTORS OF"+num1);
+		for(int j=0;j<index1;j++){
 			System.out.println(factor[j]);
+		}
+		System.out.println("PRIME FACTORS OF"+num2);
+		for(int j=0;j<index2;j++){
+			System.out.println(factorOf36[j]);
 		}
 	}
 }
