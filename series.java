f65d5232272645400fc4a5b2d2dd4f709ef38670
jasmine das <jasmine das7>
2017-06-25 21:31:33 +0530

    Find sum of all n numbers.

--- series.java
+++ series.java
@@ -2,10 +2,12 @@ class Series{
 	public static void main(String args[]){
 		 int i , j=0;
 		 for(i=1;i<=10;i++)
-		 {
+		 { 
+		 	
 		 	for(j=1;j<=i;j++){
 		 		System.out.println(i);
-		 	}
+
+		 	}System.out.println("sum of "+ i +" " + i + " times" + "="+i*i);
 		 }
 	}
 }
