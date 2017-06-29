ec77d9f1fe45e65dbde17a256570a76fd3f8d3db
jasmine das <jasmine das7>
2017-06-29 22:23:04 +0530

    Learn to read from command line

---
+++ Sum.java
@@ -0,0 +1,8 @@
+class Sum {
+	public static void main(String[] args) {
+		int number1 = Integer.parseInt(args[0]);
+		int number2 = Integer.parseInt(args[1]);
+
+		System.out.println(number1 + number2);
+	}
+}
