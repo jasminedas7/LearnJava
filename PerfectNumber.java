a9b7d26d4c836f6c02bfc04e544be0dcc03de9eb
jasmine das <jasmine das7>
2017-07-21 14:40:52 +0530

    Check a number is perfect or not

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,26 +1,53 @@
+import java.util.Scanner;
 class PerfectNumber
 {
 	public static void main(String args[]){
-		int sum=0; int i=1;
-		int num= 28;
-		while(i<=num/2)
-		{
-			if (num%i==0)
-			{
-				sum=sum+i;
-			}
-			if(sum==num)
-			{
-				System.out.println("Perfect Number");
-			}
-			else
-			{
-				System.out.println("Not Perfect Number");
-			}
-			break;
-		  }
-		}
-	}
+		        int n, sum = 0;
 
+        Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter any integer you want to check:");
 
+        n = s.nextInt();
+
+        for(int i = 1; i <= n/2; i++)
+
+        {
+
+            if(n % i == 0)
+
+            {
+
+                sum = sum + i;
+
+            }
+
+        }
+
+        if(sum == n)
+
+        {
+
+            System.out.println("Given number is Perfect");
+
+        }
+
+        else
+
+        {
+
+            System.out.println("Given number is not Perfect");
+
+        }    
+
+    }
+
+    int divisor(int x)
+
+    {
+
+       return x;
+
+    }
+
+}
